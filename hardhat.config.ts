957b862e6d57518298ace32b348fb9829eee9c84
justdev337 <>
2024-10-22 14:00:59 +0700

    added openzeppelin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,44 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  networks: {
+    base: {
+      url: "https://mainnet.base.org",
+      chainId: 8453,
+      accounts: process.env.DEPLOYER ? [process.env.DEPLOYER!] : []
+    },
+    baseSepolia: {
+      url: "https://sepolia.base.org",
+      chainId: 84532,
+      accounts: process.env.DEPLOYER ? [process.env.DEPLOYER!] : []
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.28",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
+      },
+    ]
+  },
+  sourcify: {
+    enabled: true
+  },
+  etherscan: {
+    apiKey: {
+      base: process.env.BASESCAN_API_KEY!
+    }
+  },
+  mocha: {
+    timeout: 100 * 10000000
+  },
 };
 
 export default config;
